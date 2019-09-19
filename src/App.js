40d897aac2d38eacaf70803e155f590744d89b93
Taylor Covington <tcovingtondev@gmail.com>
2019-09-18 18:54:08 -0600

    counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// added "{ Component }" above
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// lets class know it's a react component, gives state, props
+class App extends Component {
+
+  //this is how we define state
+  state = { value: 0 }
+
+  inc = () => {
+    // asynchronous - run without having to wait , runs at the same time
+    // this.setState({ value: this.state.value + 1 })
+    //the value: is what we are wanting to update
+    this.setState( (state) => {
+      return {value: state.value + 1}
+    })
+    console.log(this.state.value)
+  }
+  dec = () => {
+    // this.setState({ value: this.state.value - 1 })
+            this.setState( (state) => {
+              return {value: state.value - 1}
+            })
+  }
+
+
+  render() {
+    return (
+      <div>
+      <h1 className='colored'>Counter Example</h1>
+      <p>
+        {/* js code goes here */}
+        { this.state.value }
+      </p>
+      <button onClick={this.inc}>+</button>
+      <button onClick={this.dec}>-</button>
+      </div>
+    );
+  }
 }
 
 export default App;
